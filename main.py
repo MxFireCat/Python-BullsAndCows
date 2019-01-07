f2d671d94b52bd3375a28efeeaed769768a6edbf
lyra <mxfirecat@gmail.com>
2019-01-07 18:31:20 +1300

    made tutorial

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_delay_text(text, delay=0.0):
 
 
 def print_intro():
-    print("          .=     ,        =.")
+    print("           .=    ,        =.")
     print("  _  _   /'/    )\,/,/(_   \ \ ")
     print("   `//-.|  (  ,\ )\//\)\/_  ) |")
     print("   //___\   `\ \/ \/\/ \///'  /")
@@ -36,7 +36,6 @@ def ask_player_experience():
         print("user inputted 'yes'")  # TODO make this run the game
         return True
     elif user_input.lower() == 'n':
-        print("user inputted 'no'")  # TODO make this run the tutorial
         return False
     else:
         print("Please enter either 'y' or 'n'")
@@ -45,8 +44,15 @@ def ask_player_experience():
 
 def play_tutorial():
     time.sleep(1.5)
-    print_delay_text("That's okay. ", 1.0)
-    print_delay_text("I can help you\n")
+    print_delay_text("\nThat's okay. ", 2.0)
+    print_delay_text("I can help you...\n\n", 2.5)
+    print_delay_text("The goal of Bulls And Cows is to guess the isogram.\n", 2.6)
+    print_delay_text("Isogram meaning a world with no repeating letters.\n", 3.0)
+    print_delay_text("Every time you guess a letter that is inside the isogram\nand in the right place, you will get a Bull.\n", 4.0)
+    print_delay_text("Every time you guess a letter that is inside the isogram\nbut in the wrong place, you will get a Cow.\n", 4.0)
+    print_delay_text("Get all the letters in the right play and you win the game!\n", 2.6)
+    print_delay_text("Got it?\n", 2.4)
+    print_delay_text("Well then, get ready!\n\n")
 
 
 print_intro()
